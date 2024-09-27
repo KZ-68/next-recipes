a4f477ceb793b99108a503dc85cba598d76f2bab
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-09-27 02:49:19 +0200

    style: color properties for header nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const NavBar:React.FC<any> = () => {
   return (
-    <nav className='flex flex-row justify-between gap-3 px-4 py-7 bg-slate-900 text-white'>
-      <h1>MyRecipes</h1>
-      <div className='flex flex-row gap-3'>
+    <nav className='flex flex-row justify-between gap-3 px-20 py-7 bg-slate-800 '>
+      <h1 className='text-orange-500 text-xl'>MyRecipes</h1>
+      <div className='flex flex-row gap-6 text-white'>
         <Link href='/recipe'>Recipes</Link>
         <Link href='/article'>Blog</Link>
         <Link href='/contact'>Contact</Link>
