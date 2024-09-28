07b0c6885c8be0c70830250610f4664cdaa10e9e
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-09-28 15:02:00 +0200

    feat: update random pick record logic and change folder path

--- app/api/recipe/[recipeId]/suggestion/[categoryId]/route.ts
+++ app/api/recipe/[recipeId]/suggestion/[categoryId]/route.ts
@@ -4,8 +4,20 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest, {params} : {params : {categoryId: string}}) {
     const { categoryId } = params;
     try {
-        const recipeCount = await db.recipe.count();
-        const skip = Math.floor(Math.random() * recipeCount);
+
+        const recipe = await db.recipe.findMany({
+            "where":{
+                "categoryId": categoryId
+            },
+            "orderBy": {
+                "title": "asc"
+            }
+        })
+
+        const recipeCount = recipe.length;
+        const firstRecipeId = recipe[0];
+        const cursor = firstRecipeId.id;
+        const skip = 1 + Math.floor((recipeCount - 1) * Math.random());
 
         const category = await db.category.findUnique({
             "where": {
@@ -13,14 +25,18 @@ export async function GET(req: NextRequest, {params} : {params : {categoryId: st
             },
             "include": {
                 "recipes": {
-                    "take": 2,
+                    "cursor": {
+                        "id": cursor,
+                    },
+                    "take": 3,
                     "skip": skip,
                     "orderBy": {
                         "title": "asc"
                     },
                 }
-            }
+            },
         })
+
         return NextResponse.json(category)
     } catch (error) {
         console.log("{CATEGORY}", error)
