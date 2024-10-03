62e73d9a1e79818469aa11004eb9f39cd0ba6530
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-10-03 23:58:06 +0200

    feat: update article detail page

--- app/article/[articleId]/page.tsx
+++ app/article/[articleId]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { formatDate } from '@/lib/utils';
 import CommentBlog from '@/components/CommentBlog'
 import Tag from '@/components/Tag'
 import React, { useEffect, useState } from 'react'
@@ -17,15 +18,20 @@ const ArticleDetailPage = ({params} : {params : {articleId: string}}) => {
     }, [params.articleId])
 
     return (
-        <div>
-            <h1 className='mb-3 text-2xl'>{article?.title}</h1>
-            <p>{article?.text}</p>
-            <div className='my-5 flex flex-wrap'>
-                {article?.tags.map((tagArticle: TagArticleType) => (
-                    <Tag key={tagArticle.tag.id} text={tagArticle.tag.name}/>
-                ))}
-            </div>
-            <div className='p-4 mt-8 bg-slate-900 rounded-md'>
+        <div className='mx-10'>
+            <section className='flex flex-col items-center w-full py-36 rounded-lg bg-slate-100 bg-opacity-10'>
+                <div className='my-5 flex flex-wrap'>
+                    {article?.tags.map((tagArticle: TagArticleType) => (
+                        <Tag key={tagArticle.tag.id} text={tagArticle.tag.name}/>
+                    ))}
+                </div>
+                <h1 className='mb-3 text-5xl'>{article?.title}</h1>
+            </section>
+            <section className='px-32 py-4'>
+                <h2>Introduction</h2>
+                <p>{article?.text}</p>
+            </section>
+            <section className='py-4 px-32 mt-8 bg-slate-900 rounded-md'>
                 <h2 className='mb-4 text-xl'>Les commentaires ({article?.comments.length}) :</h2>
                 <ul>
                     {article?.comments && article.comments.length > 0 ? (
@@ -39,7 +45,7 @@ const ArticleDetailPage = ({params} : {params : {articleId: string}}) => {
                         </p>
                     )}
                 </ul>
-            </div>
+            </section>
         </div>
     )
 }
