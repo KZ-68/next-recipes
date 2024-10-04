d1ac6bba394f59873b9be452c8acbe3381248eb1
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-10-04 22:55:36 +0200

    feat: add recipe link for suggestion card

--- components/SuggestionCard.tsx
+++ components/SuggestionCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface SuggestionCardProps {
     recipe: RecipeType;
@@ -8,10 +9,12 @@ interface SuggestionCardProps {
 const SuggestionCard:React.FC<SuggestionCardProps> = ({recipe}) => {
     
     return (
-        <div className='group h-full p-6 rounded-md bg-slate-800 cursor-pointer hover:translate-y-2 duration-300' key={recipe?.id}>
-            <Image className='rounded-t-lg' src={`/images/${recipe.image_url}`} alt="Recipe Image" width="320" height="250"/>
-            {recipe.title}
-        </div>
+        <Link href={`${recipe.id}`} key={recipe.id}>
+            <div className='group h-full p-6 rounded-md bg-slate-800 cursor-pointer hover:translate-y-2 duration-300' key={recipe?.id}>
+                <Image className='rounded-t-lg' src={`/images/${recipe.image_url}`} alt="Recipe Image" width="250" height="250"/>
+                <h3>{recipe.title}</h3>
+            </div>
+        </Link>
     )
 }
 
