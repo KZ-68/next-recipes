b095126f1936e727a42fa8c470b3b2a9c215fd3d
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-10-07 23:57:38 +0200

    feat: update form data and add label

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,13 +5,11 @@ function ContactPage () {
 
     const send = async(formData:any) => {
         "use server"
-        const contactFormName = formData.get("name") as string;
         const contactFormSubject = formData.get("subject") as string;
         const contactFormBody = formData.get("body") as string;
 
         await sendMail({
             to:"nextjsrecipetest@gmail.com",
-            name: contactFormName,
             subject: contactFormSubject,
             body: contactFormBody
         })
@@ -20,10 +18,11 @@ function ContactPage () {
     return (
         <div className='flex flex-col py-8 px-6 mx-56 rounded-lg bg-slate-700'>
             <h1 className='text-2xl'>Contact Us</h1>
-            <form action={send} className='flex flex-col gap-10 py-8 px-6 rounded-2xl bg-slate-700'>
+            <form action={send} className='flex flex-col gap-6 py-8 px-6 rounded-2xl bg-slate-700'>
                 <input className='py-4 px-2 rounded-xl' disabled={true} type='text' name="to" placeholder='nextjsrecipetest@gmail.com'/>
-                <input className='text-black py-4 px-2 rounded-xl' type="text" name='name' placeholder="test"/>
-                <input className='text-black py-4 px-2 rounded-xl' type="text" name='subject'/>
+                <label>Subject :</label>
+                <input className='text-black py-4 px-2 rounded-xl' type="text" name='subject' id='subject'/>
+                <label>Text :</label> 
                 <input className='text-black py-4 px-2 rounded-xl' type="text" name='body'/>
                 <button className='py-2 px-2 rounded-xl bg-slate-800' type='submit'>Submit</button>
             </form>
