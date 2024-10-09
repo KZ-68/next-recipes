42cc16359e674b54be34f2127482f3c0e72d48de
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-10-09 23:33:59 +0200

    refactor: add formData and change input text to textarea

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,13 @@
 import { sendMail } from '@/lib/mail';
-import React from 'react'
 
 function ContactPage () {
 
-    const send = async(formData:any) => {
+    const send = async(formData:FormData) => {
         "use server"
         const contactFormSubject = formData.get("subject") as string;
         const contactFormBody = formData.get("body") as string;
 
         await sendMail({
-            to:"nextjsrecipetest@gmail.com",
             subject: contactFormSubject,
             body: contactFormBody
         })
@@ -19,11 +17,10 @@ function ContactPage () {
         <div className='flex flex-col py-8 px-6 mx-56 rounded-lg bg-slate-700'>
             <h1 className='text-2xl'>Contact Us</h1>
             <form action={send} className='flex flex-col gap-6 py-8 px-6 rounded-2xl bg-slate-700'>
-                <input className='py-4 px-2 rounded-xl' disabled={true} type='text' name="to" placeholder='nextjsrecipetest@gmail.com'/>
                 <label>Subject :</label>
                 <input className='text-black py-4 px-2 rounded-xl' type="text" name='subject' id='subject'/>
                 <label>Text :</label> 
-                <input className='text-black py-4 px-2 rounded-xl' type="text" name='body'/>
+                <textarea className='text-black py-4 px-2 rounded-xl' name='body' minLength={1} maxLength={500}/>
                 <button className='py-2 px-2 rounded-xl bg-slate-800' type='submit'>Submit</button>
             </form>
         </div>
