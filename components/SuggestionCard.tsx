899b7e084c267842670a225f411e0d557d1868dc
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-10-11 22:47:00 +0200

    feat: add placeholder image

--- components/SuggestionCard.tsx
+++ components/SuggestionCard.tsx
@@ -11,7 +11,12 @@ const SuggestionCard:React.FC<SuggestionCardProps> = ({recipe}) => {
     return (
         <Link href={`${recipe.id}`} key={recipe.id}>
             <div className='group h-full p-6 rounded-md bg-slate-800 cursor-pointer hover:translate-y-2 duration-300' key={recipe?.id}>
-                <Image className='rounded-t-lg' src={`/images/${recipe.image_url}`} alt="Recipe Image" width="250" height="250"/>
+                {recipe.image_url == "" ? (
+                    <Image className='rounded-lg h-[250px]' src={`https://placehold.co/250x250/png?text=placeholder&font=roboto`} alt="Recipe Image" width="250" height="250"/>
+                ) : 
+                (
+                    <Image className='rounded-lg h-[250px]' src={`/images/${recipe.image_url}`} alt="Recipe Image" width="250" height="250"/>
+                )}
                 <h3>{recipe.title}</h3>
             </div>
         </Link>
