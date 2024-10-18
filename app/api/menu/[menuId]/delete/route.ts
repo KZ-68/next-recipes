a8764bd154136f6791c813fc2e971f56433aef8d
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-10-18 23:52:49 +0200

    feat: add api route for GET menu list and DELETE

---
+++ app/api/menu/[menuId]/delete/route.ts
@@ -0,0 +1,31 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(req: Request, { params }: { params: { menuId: string } }) {
+    try {
+        const { menuId } = params;
+
+        // Check if the menu exists
+        const menu = await db.menu.findUnique({
+            where: {
+                id: menuId
+            }
+        });
+
+        if (!menu) {
+            return new NextResponse('Menu Not Found', { status: 404 });
+        }
+
+        // Delete the menu
+        await db.menu.delete({
+            where: {
+                id: menuId
+            }
+        });
+
+        return new NextResponse('Menu deleted successfully', { status: 200 });
+    } catch (error) {
+        console.error("[Menu]", error);
+        return new NextResponse('Internal Error', { status: 500 });
+    }
+}
---
+++ app/api/menu/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const menus = await db.menu.findMany({
+            "orderBy": {
+                "date": 'desc'
+            },
+            "include": {
+                "meals": {
+                    "orderBy": {
+                        "createdAt": "desc"
+                    },
+                    "include": {
+                        "mealrecipes": {
+                            "orderBy":{
+                                "date": "desc"
+                            },
+                            "include": {
+                                "recipe":true,
+                            }
+                        }
+                    }
+                },
+            }
+        })
+        return NextResponse.json(menus)
+    } catch (error) {
+        console.log("{MENUS}", error)
+        return new NextResponse("Internal Error", {status:500})
+    }
+}
