a07c40f48d20e45708f1ccf85e967ba6f92ea1b8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-10-18 23:50:08 +0200

    feat: add function for date format day/month/year only

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,6 +4,10 @@ export function formatDate(date: Date): string {
     return format(new Date(date), "MMMM do, yyyy HH:mm") ?? "Date not available";
 }
 
+export function formatDateCalendar(date: Date): string {
+    return format(new Date(date), "dd/M/yyyy") ?? "Date not available";
+}
+
 export const slugify = (str: string) => {
     return str.toLowerCase().trim().replace(/[\s\W-]+/g, "-").replace(/^-+|-+$/g, "");
 }
