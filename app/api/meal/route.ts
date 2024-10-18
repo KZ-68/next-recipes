91cae2fc0e0523243f7f12e404e5668b9d3d22cb
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-10-18 23:53:51 +0200

    feat: add api route for GET meals list

---
+++ app/api/meal/route.ts
@@ -0,0 +1,23 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const meals = await db.meal.findMany({
+            "orderBy": {
+                "createdAt": 'desc'
+            },
+            "include": {
+                "mealrecipes":{
+                    "orderBy": {
+                        "date": "desc"
+                    }
+                }
+            }
+        })
+        return NextResponse.json(meals)
+    } catch (error) {
+        console.log("{MEALS}", error)
+        return new NextResponse("Internal Error", {status:500})
+    }
+}
