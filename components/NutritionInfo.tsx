a2cf6c26c2b12b7075423d8910e2103be13d6776
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-10-26 23:25:38 +0200

    feat: update NutritionInfo component

--- components/NutritionInfo.tsx
+++ components/NutritionInfo.tsx
@@ -1,45 +1,77 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface NutritionInfoProps {
-    recipe: RecipeType
+    energyLabel: string
+    energyQuantity: string
+    energyUnit: string
+    totalFatLabel: string
+    totalFatQuantity: string
+    totalFatUnit: string
+    carbohydrateLabel: string
+    carbohydrateQuantity: string
+    carbohydrateUnit: string
+    protLabel: string
+    protQuantity: string
+    protUnit: string
+    sugarLabel: string
+    sugarQuantity: string
+    sugarUnit: string
+    vitcLabel: string
+    vitcQuantity: string
+    vitcUnit: string
 }
 
-const NutritionInfo:React.FC<NutritionInfoProps> = ({recipe}) => {
-
-    const [recipeState, setRecipeState] = useState<RecipeType>(recipe)
-    const [nutrionState, setNutritionState] = useState(null)
-            
-    const fetchIngredientsNutrition = async () => {
-        try {
-            const EDAMAM_APPID = process.env.NEXT_PUBLIC_EDAMAM_APPID;
-            const EDAMAM_KEY = process.env.NEXT_PUBLIC_EDAMAM_KEY;
-            
-            const headers = {
-                "Content-Type": "application/json"
-            }
-            
-            const response = await fetch(`https://api.edamam.com/api/nutrition-details?app_id=${EDAMAM_APPID}&app_key=${EDAMAM_KEY}`, {
-                method: 'POST',
-                headers: headers,
-                body: JSON.stringify({
-                    'title': recipeState.title,
-                    'ingr': recipeState.ingredients
-                }),
-            })
-            if(response.ok) {
-                const nutritionalDetailsData = await response.json()
-                console.log(nutritionalDetailsData);
-                setNutritionState(nutritionalDetailsData);
-            }
-        } catch(error) {
-            console.error("Error submitting data", error);
-        }
+const NutritionInfo:React.FC<NutritionInfoProps> = (
+    {
+        energyLabel, 
+        energyQuantity, 
+        energyUnit, 
+        totalFatLabel, 
+        totalFatQuantity, 
+        totalFatUnit,
+        carbohydrateLabel,
+        carbohydrateQuantity,
+        carbohydrateUnit,
+        protLabel,
+        protQuantity,
+        protUnit,
+        sugarLabel,
+        sugarQuantity,
+        sugarUnit,
+        vitcLabel,
+        vitcQuantity,
+        vitcUnit
     }
+) => {
 
-    fetchIngredientsNutrition()
-  return (
-    <div></div>
-  )
+    return (
+        <div className='flex flex-row flex-wrap justify-center gap-2 py-2 px-2 bg-slate-700 rounded-md'>
+            <div className='flex flex-col items-center py-8 px-14 rounded-md bg-slate-900 w-[32%]'>
+                <h3>{energyLabel}</h3>
+                <p className='flex flex-row gap-3'>{energyQuantity}{energyUnit}</p>
+            </div>
+            <div className='flex flex-col items-center py-8 px-14 rounded-md bg-slate-900 w-[32%]'>
+                <h3>{protLabel}</h3>
+                <p className='flex flex-row gap-3'>{protQuantity}{protUnit}</p>
+            </div>
+            <div className='flex flex-col items-center py-8 px-14 rounded-md bg-slate-900 w-[32%]'>
+                <h3>{carbohydrateLabel}</h3>
+                <p className='flex flex-row gap-3'>{carbohydrateQuantity}{carbohydrateUnit}</p>
+            </div>
+            <div className='flex flex-col items-center py-8 px-14 rounded-md bg-slate-900 w-[32%]'>
+                <h3>{totalFatLabel}</h3>
+                <p className='flex flex-row gap-3'>{totalFatQuantity}{totalFatUnit}</p>
+            </div>
+            <div className='flex flex-col items-center py-8 px-14 rounded-md bg-slate-900 w-[32%]'>
+                <h3>{sugarLabel}</h3>
+                <p className='flex flex-row gap-3'>{sugarQuantity}{sugarUnit}</p>
+            </div>
+            <div className='flex flex-col items-center py-8 px-14 rounded-md bg-slate-900 w-[32%]'>
+                <h3>{vitcLabel}</h3>
+                <p className='flex flex-row gap-3'>{vitcQuantity}{vitcUnit}</p>
+            </div>
+        </div>
+    )
 }
 
 export default NutritionInfo
