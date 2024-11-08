1e0150eb59784597fd7dfd1e5700494fc3f503e8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-11-08 23:40:34 +0100

    feat: update dark mode properties

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "selector",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,12 +9,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        
-      },
     },
   },
-  plugins: [],
-  darkMode: ['class','.dark-theme']
+  plugins: []
 };
 export default config;
