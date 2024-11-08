ab4f6011a6bf08e7a2271ac9a62be76e431733ac
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-11-08 23:53:06 +0100

    refactor: update meal recipes data

--- components/DraggableItem.tsx
+++ components/DraggableItem.tsx
@@ -1,14 +1,18 @@
 import Draggable, {DraggableCore} from 'react-draggable';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { GripVerticalIcon, Trash2Icon } from 'lucide-react';
 import { Button } from '@nextui-org/react';
 
 interface DraggableItemProps {
-    meal: MealType,
-    mealrecipe: MealRecipeType
+    recipes: RecipeType[],
+    recipe: RecipeType
+    setRecipes: React.Dispatch<React.SetStateAction<RecipeType[]>>
 }
 
-const DraggableItem:React.FC<DraggableItemProps> = ({meal, setmeal, mealrecipe}) => {
+const DraggableItem:React.FC<DraggableItemProps> = ({recipes, recipe, setRecipes}) => {
+
+    const nodeRef = useRef(null);
+    const dragAreaRef = useRef<HTMLDivElement>(null);
 
     const eventLogger = (e: MouseEvent, data: Object) => {
         console.log('Event: ', e);
@@ -16,15 +20,25 @@ const DraggableItem:React.FC<DraggableItemProps> = ({meal, setmeal, mealrecipe})
     };
     
     return (
-        <Draggable axis='y' onTouchEnd={eventLogger} scale={1}>
-            <li className='flex flex-row justify-start gap-2 py-3 px-4 rounded-md bg-slate-800'>
-                <GripVerticalIcon />
-                <p>{mealrecipe.recipe.title}</p>
-                <Button className="flex items-center text-red-500 ml-auto" onPress={setmeal}>
-                    <Trash2Icon size={20} />
-                </Button>
-            </li>
-        </Draggable>
+        <div ref={dragAreaRef}>
+            <Draggable axis='y' onTouchEnd={eventLogger} scale={1} nodeRef={nodeRef}>
+                <li ref={nodeRef} draggable={true} className='flex flex-row justify-start gap-2 py-3 px-4 rounded-md bg-slate-800'>
+                    <GripVerticalIcon className='cursor-pointer' />
+                    <p>{recipe.title}</p>
+                    <Button className="flex items-center text-red-500 ml-auto" 
+                    onPress={() => {
+                        const newRecipesList = recipes.filter((recipesItem) => {
+                            if (recipesItem.id != recipe.id) {
+                            return recipesItem;
+                            }
+                        });
+                        setRecipes(newRecipesList);
+                    }}>
+                        <Trash2Icon size={20} />
+                    </Button>
+                </li>
+            </Draggable>
+        </div>
     )
 }
  
