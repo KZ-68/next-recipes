b88d72950ece4fba167b29d68da2a49f1bfd7cce
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-11-15 23:04:05 +0100

    style: update button component to link properties

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { formatDate } from '@/lib/utils'
-import Button from './Button'
 import Tag from './Tag'
 import { useRouter } from 'next/navigation';
 import { Trash2Icon } from 'lucide-react';
+import Link from 'next/link'
 
 interface ArticleCardProps {
     article: ArticleWithTagsAndComments;
@@ -33,17 +33,19 @@ const ArticleCard:React.FC<ArticleCardProps> = ({article}) => {
         <div className='group border border-slate-500 p-6 rounded-md cursor-pointer hover:translate-y-2 duration-300 md:h-full' key={article.id}>
             {/* Titre de l'article */}
             <h2 className='text-2xl md:text-xl font-bold'>{article.title}</h2>
-            <p className='text-sm text-slate-300'>{formatDate(article.createdAt)}</p>
+            <p className='text-sm text-slate-300 my-2'>{formatDate(article.createdAt)}</p>
             <div className='flex flex-wrap gap-2 my-3 md:leading-8'>
-                {article.tags.map((tagArticle:any)=> (
+                {article.tags.map((tagArticle:TagArticleType)=> (
                     <Tag text={tagArticle.tag.name} key={tagArticle.tag.id}/>
                 ))}
             </div>
             
             {/* Texte de l'article */}
-            <p className='line-clamp-4 py-8'>{article.text}</p>
+            <p className='line-clamp-4 py-1 my-4 text-sm'>{article.text}</p>
 
-            <Button href="https://google.fr" label="Read more..."/>
+            <div className='my-6'>
+                <Link className='text-orange-600' href="https://google.fr">Read more...</Link>
+            </div>
             <div className='sm:top-5 sm:right-5 my-4'>
                 <button className="flex gap-2 px-5 py-2 rounded-md bg-red-500 hover:bg-red-600 text-xs" 
                 onClick={handleDelete}><Trash2Icon size={15} />Delete</button>
