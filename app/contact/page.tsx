e83c4b2794ada5c06d67a5d502535b5fffe56b81
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-11-15 23:02:03 +0100

    feat: add email adress field and data type

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,17 @@
 "use client"
 import { sendMail } from '@/lib/mail';
-import { CheckCircle2 } from 'lucide-react';
+import { CheckCircle2, XCircleIcon } from 'lucide-react';
 import { useFormState } from 'react-dom';
 
 async function sendMessage(prevState, formData:FormData){
     
+    const contactFormEmail = formData.get("email") as string;
     const contactFormSubject = formData.get("subject") as string;
     const contactFormBody = formData.get("body") as string;
 
     
     const sendmailResponse = await sendMail({
+        email: contactFormEmail,
         subject: contactFormSubject,
         body: contactFormBody
     })
@@ -36,16 +38,18 @@ function ContactPage () {
                 <p id="success" className='bg-white w-fit py-2 px-3 my-3 text-black rounded-md'><CheckCircle2 className='flex flex-row gap-2 text-white bg-green-700 rounded-full'/>{formState?.message}</p>
             )}
             {formState?.success === false && (
-                <p id="error">{formState?.message}</p>
+                <p id="error" className='bg-white w-fit py-2 px-3 my-3 text-black rounded-md'><XCircleIcon className='text-white bg-red-600 rounded-full'/>{formState?.message}</p>
             )}
-            <div className='flex flex-col py-8 px-6 mx-56 rounded-lg bg-slate-700'>
+            <div className='flex flex-col py-8 px-6 mx-80 rounded-lg bg-slate-800'>
                 <h1 className='text-2xl'>Contact Us</h1>
-                <form action={formAction} className='flex flex-col gap-6 py-8 px-6 rounded-2xl bg-slate-700'>
-                    <label>Subject :</label>
-                    <input className='text-black bg-white py-4 px-2 rounded-xl' type="text" name='subject' id='subject'/>
-                    <label>Text :</label> 
-                    <textarea className='text-black bg-white py-4 px-2 rounded-xl' name='body' minLength={1} maxLength={500}/>
-                    <button className='py-2 px-2 rounded-xl bg-slate-800' type='submit'>Submit</button>
+                <form action={formAction} className='flex flex-col gap-6 py-8 px-6 rounded-2xl'>
+                    <label>Email</label>
+                    <input placeholder='Enter your email' className='light:text-black text-white bg-slate-600 py-4 px-4 rounded-lg' type="text" name='email' id='email'/>
+                    <label>Subject</label>
+                    <input placeholder='Enter the subject' className='light:text-black text-white bg-slate-600 py-4 px-4 rounded-lg' type="text" name='subject' id='subject'/>
+                    <label>Message</label> 
+                    <textarea placeholder='Type your message here...' className='light:text-black text-white bg-slate-600 py-4 px-4 rounded-lg' name='body' minLength={1} maxLength={500} rows={6}/>
+                    <button className='py-2 px-2 rounded-lg bg-indigo-500' type='submit'>Submit</button>
                 </form>
             </div>
         </>
