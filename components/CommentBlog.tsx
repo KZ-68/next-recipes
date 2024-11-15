4745138f8829222a0f871e6a83cddd1bb2f306d8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-11-15 23:04:46 +0100

    style: update paddings and margins

--- components/CommentBlog.tsx
+++ components/CommentBlog.tsx
@@ -18,20 +18,21 @@ const Comment:React.FC<CommentProps> = ({comment, article}) => {
             const res = await fetch(`/api/comment-blog/${comment.id}/delete`, {
                 method: 'DELETE'
             })
-
-            location.reload();
+            if(res.ok) {
+                location.reload();
+            }
         } catch(error) {
-            console.error("Error deleting comment")
+            console.error(`Error deleting comment : ${error}`)
         }
     }
 
     return (
-        <li key={comment.id} className='flex-row m-5 group border border-slate-500 p-6 rounded-sm'>
+        <li key={comment.id} className='flex-row m-5 group border-2 border-slate-500 p-6 rounded-md'>
             <div className='flex flex-col gap-1'>
                 <p className='text-sm text-slate-300'>{formatDate(article.createdAt)}</p>
-                <p>{comment.text}</p>
+                <p className='my-3'>{comment.text}</p>
             </div>
-            <div className='sm:top-5 sm:right-5 my-4'>
+            <div className='sm:top-5 sm:right-5 my-2'>
                 <button className="flex gap-2 px-5 py-2 rounded-md bg-red-500 hover:bg-red-600 text-xs" 
                 onClick={handleDelete}><Trash2Icon size={15} />Delete</button>
             </div>
