cbf4f4c95710760d3b0427b99fe458ec8188c4dd
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-11-22 00:05:12 +0100

    refactor: update api method arguments

--- app/api/recipe/[recipeId]/comments/route.ts
+++ app/api/recipe/[recipeId]/comments/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { recipeId: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { recipeId: string } }) {
     try {
         const { recipeId } = params;
 
