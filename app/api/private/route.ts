9f308fd72a8357a4149331ab02f0c2290acdb1b3
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-11-22 11:00:20 +0100

    refactor: update condition for null value for current user connected

--- app/api/private/route.ts
+++ app/api/private/route.ts
@@ -3,20 +3,27 @@ import { clerkClient, currentUser } from '@clerk/nextjs/server'
 
 export async function POST(req:NextRequest) {
     const user = await currentUser()
-    const userId = user.id;
-    const body = await req.json()
-    await clerkClient.users.updateUserMetadata(userId, {
-    privateMetadata: {
-      favoriteRecipes: body.recipe,
-    },
-  })
-  return NextResponse.json({ success: true })
+    if(user === null) {
+      return NextResponse.json({ success: false })
+    } else {
+      const userId = user.id;
+      const body = await req.json()
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          favoriteRecipes: body.recipe,
+        },
+      })
+      return NextResponse.json({ success: true })
+    }
 }
 
 export async function GET() {
-    const currentUserData = await currentUser()
+  const currentUserData = await currentUser()
+  if(currentUserData === null) {
+    return NextResponse.json({ success: false })
+  } else {
     const userId = currentUserData.id;
-
     const user = await clerkClient.users.getUser(userId)
     return NextResponse.json(user.privateMetadata)
+  }
 }
