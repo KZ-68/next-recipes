b9483899e7d4ff7744d9b345e564f7ddc7c59c97
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-11-29 01:20:52 +0100

    feat: add limitations by positions

--- components/DraggableItem.tsx
+++ components/DraggableItem.tsx
@@ -1,33 +1,45 @@
-import Draggable from 'react-draggable';
-import React, { useRef } from 'react'
-import { GripVerticalIcon, Trash2Icon } from 'lucide-react';
-import { Button } from '@nextui-org/react';
+import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
+import React, { useRef, useState } from 'react'
+import { GripVerticalIcon } from 'lucide-react';
 
 interface DraggableItemProps {
     meal: MealType
+    recipes: RecipeType[]
     recipe: RecipeType
     setMeal: React.Dispatch<React.SetStateAction<MealType | null>>
 }
 
-const DraggableItem:React.FC<DraggableItemProps> = ({meal, recipe, setMeal}) => {
+const DraggableItem:React.FC<DraggableItemProps> = ({meal, recipes, recipe, setMeal}) => {
 
     const nodeRef = useRef(null);
     const dragAreaRef = useRef<HTMLDivElement>(null);
+
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    const itemHeight = 50;
+    const listHeight = recipes.length * itemHeight;
+    const listWidth = 400;
+    const itemWidth = 390;
+
+    const maxY = listHeight - itemHeight;
+    const maxX = listWidth - itemWidth;
+
+    const handleDrag = (e:DraggableEvent, data:DraggableData) => {
+        const clampedX = Math.min(Math.max(data.x, 0), maxX);
+        const clampedY = Math.min(Math.max(data.y, 0), maxY);
+        setPosition({ x: clampedX, y: clampedY });
+    };
     
     return (
         <div ref={dragAreaRef}>
-            <Draggable axis='y' scale={1} nodeRef={nodeRef}>
+            <Draggable scale={1}
+                axis='y'
+                position={position}
+                onDrag={handleDrag}
+            >
                 <li ref={nodeRef} draggable={true} className='flex flex-row justify-start gap-2 py-3 px-4 rounded-md bg-slate-800'>
                     <GripVerticalIcon className='cursor-pointer' />
-                    <p>{recipe.title}</p>
-                    <Button className="flex items-center text-red-500 ml-auto" 
-                    onPress={() => {
-                       meal.mealrecipes = [];
-                       setMeal(meal);
-                       meal.mealrecipes.length = 0;
-                    }}>
-                        <Trash2Icon size={20} />
-                    </Button>
+                    <p>{recipe.title}</p>    
                 </li>
             </Draggable>
         </div>
