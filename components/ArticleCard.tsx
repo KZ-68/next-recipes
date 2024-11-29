8b12ae9e5d9e508f4b5c92a6590c4f3535f8da19
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-11-29 02:47:41 +0100

    style: update css and tailwind properties

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -30,7 +30,7 @@ const ArticleCard:React.FC<ArticleCardProps> = ({article}) => {
     }
 
     return (
-        <div className='group border dark:border-slate-500 bg-white p-6 rounded-md cursor-pointer hover:translate-y-2 duration-300 md:h-full' key={article.id}>
+        <div className='group border border-slate-500 dark:bg-white p-6 rounded-md cursor-pointer hover:translate-y-2 duration-300 md:h-full' key={article.id}>
             {/* Titre de l'article */}
             <h2 className='text-2xl md:text-xl font-bold text-black dark:text-white'>{article.title}</h2>
             <p className='text-sm dark:text-slate-300 text-slate-800 my-2'>{formatDate(article.createdAt)}</p>
@@ -41,10 +41,10 @@ const ArticleCard:React.FC<ArticleCardProps> = ({article}) => {
             </div>
             
             {/* Texte de l'article */}
-            <p className='line-clamp-4 py-1 my-4 text-sm text-black dark:text-white'>{article.text}</p>
+            <p className='line-clamp-4 py-1 my-4 text-sm text-black '>{article.text}</p>
 
             <div className='my-6'>
-                <Link className='text-orange-600' href="https://google.fr">Read more...</Link>
+                <Link className=' text-orange-600' href="https://google.fr">Read more...</Link>
             </div>
             <div className='sm:top-5 sm:right-5 my-4'>
                 <button className="flex gap-2 px-5 py-2 rounded-md bg-red-500 hover:bg-red-600 text-xs" 
