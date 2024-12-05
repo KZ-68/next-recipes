6506004d535989786dcbe5637f15980304b53047
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-12-05 23:50:32 +0100

    feat: add privacy policy page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,93 @@
+"use client"
+import React from 'react'
+
+const PrivacyPoliciesPage = () => {
+    return(
+        <div className='flex flex-col gap-4 my-6'>
+            <h1>Politique de Confidentialité de MyRecipes</h1>
+
+            <h2>Date de mise à jour : [Date]</h2>
+
+            
+            <p>                    
+                Chez MyRecipes, nous nous engageons à protéger votre vie privée et à garantir la sécurité de vos informations personnelles.<br/>
+                Cette politique de confidentialité décrit les informations que nous collectons, <br/>
+                comment nous les utilisons et vos droits concernant vos données personnelles.
+            </p>
+
+            <section>
+                <h3>1. Collecte des informations personnelles</h3>
+                Nous collectons des informations lorsque vous :
+
+                Créez un compte sur notre site ;
+                Vous inscrivez à notre newsletter ;
+                Partagez des recettes ou interagissez avec notre communauté ;
+                Nous contactez via notre formulaire de contact.
+                Les types d&apos;informations collectées peuvent inclure :
+
+                Informations personnelles identifiables : nom, prénom, adresse e-mail, numéro de téléphone ;
+                Informations non identifiables : adresse IP, type de navigateur, pages visitées, et autres informations techniques.
+            </section>
+            
+            <section>
+                <h3>2. Utilisation des informations</h3>
+                Les informations collectées sont utilisées pour :
+
+                Vous fournir des services et une expérience personnalisée ;
+                Améliorer la fonctionnalité de notre site ;
+                Répondre à vos demandes et commentaires ;
+                Vous envoyer des informations marketing et des mises à jour, avec votre consentement.
+            </section>
+            
+            <section>
+                <h3>3. Partage des informations</h3>
+                Nous ne partageons pas vos informations personnelles avec des tiers, sauf dans les cas suivants :
+
+                Partenaires de service : Nous pouvons partager vos informations avec des prestataires de services de confiance qui nous aident à gérer notre site ou à fournir des services (ex : hébergement, marketing, etc.). Ces prestataires sont tenus de respecter la confidentialité de vos données.
+                Exigences légales : Nous pouvons divulguer vos informations personnelles si cela est requis par la loi ou dans le cadre d&apos;une procédure judiciaire.
+            </section>
+            
+            <section>
+                <h3>4. Cookies</h3>
+                Nous utilisons des cookies pour améliorer votre expérience sur notre site. Les cookies sont de petits fichiers stockés sur votre appareil. Ils nous aident à analyser l&apos;utilisation du site et à personnaliser votre expérience. Vous pouvez gérer ou désactiver les cookies dans les paramètres de votre navigateur.
+            </section>
+
+            <section>
+                <h3>5. Sécurité des données</h3>
+                Nous mettons en place des mesures de sécurité appropriées pour protéger vos informations personnelles contre l&apos;accès non autorisé, la modification, la divulgation ou la destruction.
+            </section>
+
+            <section>
+                <h3>6. Conservation des données</h3>
+                Nous conservons vos informations personnelles uniquement pendant la durée nécessaire pour remplir les objectifs décrits dans cette politique. Les informations peuvent être conservées plus longtemps si cela est requis par la loi ou pour des raisons légales.
+            </section>
+
+            <section>
+                <h3>7. Vos droits</h3>
+                Conformément au Règlement Général sur la Protection des Données (RGPD), vous disposez des droits suivants concernant vos données personnelles :
+
+                Droit d&apos;accès : Vous pouvez demander à consulter les informations que nous avons collectées à votre sujet ;<br/>
+                Droit de rectification : Vous pouvez demander la correction de vos informations si elles sont inexactes ou incomplètes ;<br/>
+                Droit à l&apos;effacement : Vous pouvez demander la suppression de vos données personnelles ;<br/>
+                Droit d&apos;opposition : Vous pouvez vous opposer au traitement de vos données à des fins de marketing direct ;<br/>
+                Droit à la portabilité : Vous pouvez demander à recevoir vos données personnelles dans un format structuré, couramment utilisé et lisible par machine.<br/>
+                Pour exercer ces droits, veuillez nous contacter à l&apos;adresse suivante : [Adresse e-mail de contact].<br/>
+            </section>
+
+            <section>
+                <h3>8. Modifications de cette politique</h3>
+                Nous pouvons mettre à jour cette politique de confidentialité de temps en temps. Les modifications seront publiées sur cette page avec une date de mise à jour révisée. Nous vous encourageons à consulter régulièrement cette page pour rester informé de la manière dont nous protégeons vos informations.
+            </section>
+            
+            <section>
+                <h3>9. Contact</h3>
+                Si vous avez des questions concernant cette politique de confidentialité ou si vous souhaitez exercer vos droits, n&apos;hésitez pas à nous contacter à l&apos;adresse suivante :
+
+                E-mail : [Votre adresse e-mail de contact]
+                Adresse postale : [Votre adresse postale]
+            </section>
+        </div>
+    )
+}
+
+export default PrivacyPoliciesPage
