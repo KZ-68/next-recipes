e3b62eb9aa377fc83110f4bcebe713ff8f59c6fd
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-12-12 23:57:00 +0100

    feat: move GET method into seperate file

--- app/api/private/favorite/route.ts
+++ app/api/private/favorite/route.ts
@@ -1,30 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import { clerkClient, currentUser } from '@clerk/nextjs/server'
 
-export async function POST(req:NextRequest) {
-    const user = await currentUser()
-    if(user === null) {
-      return NextResponse.json({ success: false })
-    } else {
-      const userId = user.id;
-      const body = await req.json()
-      await clerkClient.users.updateUserMetadata(userId, {
-        privateMetadata: {
-          favoriteRecipes: [body.recipe],
-        },
-      })
-      return NextResponse.json({ success: true })
-    }
-}
-
 export async function GET() {
-  const currentUserData = await currentUser()
-  if(currentUserData === null) {
-    return NextResponse.json({ success: false })
-  } else {
-    const userId = currentUserData.id;
-    const user = await clerkClient.users.getUser(userId)
-    console.log(user.privateMetadata);
-    return NextResponse.json(user.privateMetadata)
-  }
+    const currentUserData = await currentUser()
+    if(currentUserData === null) {
+        return NextResponse.json({ success: false })
+    } else {
+        const userId = currentUserData.id;
+        const user = await clerkClient.users.getUser(userId)
+        return NextResponse.json(user.privateMetadata)
+    }
 }
