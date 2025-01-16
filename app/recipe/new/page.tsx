d083e9fffc88fa4198ed51994e7d1a97a4ff383e
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-01-16 23:59:29 +0100

    refactor: move initial function into a component

--- app/recipe/new/page.tsx
+++ app/recipe/new/page.tsx
@@ -1,50 +1,26 @@
 "use client"
 import React from 'react'
 import ThemeSwitcherScroll from '@/components/ThemeSwitcherScroll';
+import { CheckCircle2, XCircleIcon } from 'lucide-react';
+import { recipeFormAction } from "@/components/newRecipeForm";
 import { useFormState } from 'react-dom';
 
-async function newRecipeForm(prevState: { message: string }, formData:FormData) {
-
-    const recipeTitle = formData.get("title") as string;
-    const recipeInstruction = formData.get("instruction") as string;
-    const recipeSlug = formData.get("slug") as string;
-    const recipeImageUrl = formData.get("image_url") as string;
-    const recipeImageUrlCloud = formData.get("image_url_cloud") as string;
-    const recipeVegan = formData.get("vegan");
-    const recipeHealthy = formData.get("healthy");
-    const recipeRating = formData.get("rating");
-    const recipeDuration = formData.get("duration");
-    const bodyForm = ({
-        title: recipeTitle,
-        instruction: recipeInstruction, 
-        slug: recipeSlug,
-        image_url: recipeImageUrl,
-        image_url_cloud: recipeImageUrlCloud,
-        vegan: recipeVegan,
-        healthy: recipeHealthy,
-        rating: recipeRating,
-        duration: recipeDuration
-    })
-    try {
-        await fetch(`/api/recipe/new`, {
-            method: 'POST',
-            body: JSON.stringify({
-                recipe : bodyForm
-            }),
-        })
-        alert(`Added "${recipeTitle}"`);
-    } catch (err) {
-        if (err instanceof Error) {
-            return err.toString();
-        }
-    }
+const initialState = {
+    message: '',
+    success: false
 }
 
 const NewRecipePage = () => {
-    const [formState, formAction] = useFormState(newRecipeForm, null);
+    const [formState, formAction] = useFormState(recipeFormAction, initialState);
     return (
     <>
         <ThemeSwitcherScroll/>
+        {formState?.success === true && (
+            <p id="success" className='bg-white w-fit py-2 px-3 my-3 text-black rounded-md'><CheckCircle2 className='flex flex-row gap-2 text-white bg-green-700 rounded-full'/>{formState?.message}</p>
+        )}
+        {formState?.success === false && (
+            <p id="error" className='bg-white w-fit py-2 px-3 my-3 text-black rounded-md'><XCircleIcon className='text-white bg-red-600 rounded-full'/>{formState?.message}</p>
+        )}
         <h1 className='text-2xl text-white'>New Recipe</h1>
         <p className='text-white'>With this form, you can add a new recipe</p>
         <div className='flex flex-col py-8 px-6 mx-80 rounded-lg bg-slate-800'>
@@ -56,9 +32,9 @@ const NewRecipePage = () => {
                 <label className='text-white' htmlFor="title">Slug : </label>
                 <input type='text' name="recipe-slug" id="recipe-slug" placeholder="Add a slug..." />
                 <label className='text-white' htmlFor="duration">Duration : </label>
-                <input type='number' min="1" max="1000" name="recipe-duration" id="recipe-duration" />
+                <input type='number' min="1" max="1000" name="recipe-duration" id="recipe-duration" placeholder='1' />
                 <label className='text-white' htmlFor="rating">Rating : </label>
-                <input type='number' name="recipe-rating" id="recipe-rating"/>
+                <input type='number' min="0" max="5" name="recipe-rating" id="recipe-rating" placeholder='0'/>
                 <fieldset>
                     <legend className='text-white'>Vegan ?</legend>
                     <input type="radio" id="vegan-true" name="vegan" value="true" />
@@ -74,7 +50,6 @@ const NewRecipePage = () => {
                     <label htmlFor="false">False</label><br />
                 </fieldset>
                 <button className='py-2 px-2 rounded-lg bg-indigo-500 text-white' type='submit'>Add</button>
-                {!!formState && <p>{formState}</p>}
             </form>
         </div>
         
