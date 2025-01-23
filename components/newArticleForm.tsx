e1ffe4cd64a41742213f4c5f94f71e0f4b2197ca
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-01-23 23:38:19 +0100

    feat: add new article process file

---
+++ components/newArticleForm.tsx
@@ -0,0 +1,33 @@
+"use server"
+
+export async function articleFormAction(prevState: { success: boolean; message: string; } | Promise<{ success: boolean; message: string; } | null> | null, formData:FormData) {
+    prevState = null;
+    
+    const articleTitle = formData.get("title") as string;
+    const articleText = formData.get("text") as string;
+    const articleSlug = formData.get("slug") as string;
+    const bodyForm = ({
+        title: articleTitle,
+        text: articleText,
+        slug: articleSlug,
+    })
+    
+    const response = await fetch(process.env.URL + `/api/article/new`, {
+        method: 'POST',
+        body: JSON.stringify({
+            article : bodyForm
+        }),
+    })
+
+    if(response.ok) {
+        return {
+            success: true,
+            message: 'Article added successfully !',
+        };
+    } else {
+        return {
+            success: false,
+            message: 'Error during the article creation process',
+        };
+    }
+}
